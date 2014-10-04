ef9f8fe4a9f8b83e4d20e29a203da7405555f927
Bruno Rocha <brunorocha@Brunos-MacBook-Pro-2.local>
2014-10-04 16:20:56 -0300

    Initial Commit

---
+++ FyberChallenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FyberChallenge
+//
+//  Created by Bruno Rocha on 10/4/14.
+//  Copyright (c) 2014 Bruno Rocha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FyberChallenge/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FyberChallenge
+//
+//  Created by Bruno Rocha on 10/4/14.
+//  Copyright (c) 2014 Bruno Rocha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
